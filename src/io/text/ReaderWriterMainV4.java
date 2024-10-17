e4e49b199117436b0fefc2b2f90ffdc08c4f472e
Seunghyub Choi <seunghyub.choi@gmail.com>
2024-10-17 22:27:58 +0900

    bufferedwriter/reader

---
+++ src/io/text/ReaderWriterMainV4.java
@@ -0,0 +1,41 @@
+package io.text;
+
+import java.io.*;
+
+import static io.text.TextConst.FILE_NAME;
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+public class ReaderWriterMainV4 {
+    private static final int BUFFER_SIZE = 8192;
+
+    public static void main(String[] args) throws IOException {
+        String writeString = "ABC\n가나다";
+        System.out.println("write String : " + writeString);
+
+        // 파일에 쓰기
+        FileWriter fw = new FileWriter(FILE_NAME, UTF_8);
+        BufferedWriter bw = new BufferedWriter(fw, BUFFER_SIZE);
+       /* fw.write(writeString);
+        fw.close();*/
+        bw.write(writeString);
+        bw.close();
+
+
+        StringBuilder content = new StringBuilder();
+        FileReader fr = new FileReader(FILE_NAME, UTF_8);
+        BufferedReader br = new BufferedReader(fr, BUFFER_SIZE);
+
+        String line;
+        while( (line = br.readLine()) != null) {
+            content.append(line).append("\n");
+        }
+        br.close();
+
+/*        int ch;
+        while ((ch = fr.read()) != -1) {
+            content.append((char) ch);
+        }*/
+        //fr.close();
+        System.out.println(content.toString());
+    }
+}
