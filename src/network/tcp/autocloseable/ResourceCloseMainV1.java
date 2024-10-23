6d47696c538e55f0e48aa69d6a462c7170971b7a
Seunghyub Choi <seunghyub.choi@gmail.com>
2024-10-24 00:00:46 +0900

    multi socket

---
+++ src/network/tcp/autocloseable/ResourceCloseMainV1.java
@@ -0,0 +1,25 @@
+package network.tcp.autocloseable;
+
+public class ResourceCloseMainV1 {
+    public static void main(String[] args) {
+        try {
+            logic();
+        } catch (CallException e) {
+            throw new RuntimeException(e);
+        } catch (CloseException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void logic() throws CallException, CloseException {
+        ResourceV1 resource1 = new ResourceV1("resource1");
+        ResourceV1 resource2 = new ResourceV1("resource2");
+
+        resource1.call();
+        resource2.callEx();
+
+        System.out.println("자원 정리");
+        resource2.closeEx();
+        resource1.close();
+    }
+}
